ab3b5204d75451d52eab33fd0563b27516874386
Thi <anhthix7c@gmail.com>
2024-01-30 22:03:19 +0700

    update admin client

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
-const base_url = 'http://localhost:4000/api/v1/';
+import axios from "axios";
+const base_url = "https://website-ec.vercel.app/api/v1/";
 
 const instance = axios.create({
   baseURL: base_url,
