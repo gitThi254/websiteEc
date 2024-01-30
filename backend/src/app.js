3808d03b039c3b5ce9ee7ef16016738ac040fc6c
Thi <anhthix7c@gmail.com>
2024-01-30 23:25:04 +0700

    update admin

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,11 +23,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://website-ec-admin.vercel.app",
-    ],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
